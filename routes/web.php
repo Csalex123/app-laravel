2b3ba1e80f14dcf46489315949c51cebefa4627b
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-08-27 20:39:34 -0300

    Routa do tipo any e match

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 
+Route::any('/any', function(){
+    return 'Recebe todos os tipos de dados';
+});
+
+Route::match(['post', 'get'], '/match', function() {
+    return 'Receberá apenas os métodos passados no array.';
+});
+
 Route::get('/contato', function() {
     return view('contact');
 });
