0ab52331bf497b5ac8728fa7f3d0cbf3fa451562
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-08-27 21:01:09 -0300

    Rota com paramentros

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,36 @@
 <?php
 
 
+
+/* Paramentros na routa */
+
+Route::get('produto/{idProduto?}', function($idProduto = ''){
+    return "Produtos {$idProduto}";
+});
+
+Route::get('/categorias/{paramentro}', function($paramentroUrl){
+    return "Esse é o paramentro da url: {$paramentroUrl}";
+});
+
+Route::get('/categoria/{flag}/post', function($flag) {
+    return "A flag que pegamos no paramentro é essa: {$flag}";
+});
+
+/* Any e Match */
+
 Route::any('/any', function(){
     return 'Recebe todos os tipos de dados';
 });
 
 Route::match(['post', 'get'], '/match', function() {
     return 'Receberá apenas os métodos passados no array.';
+});
+
+
+/* GET E POST */
+
+Route::post('/receber', function() {
+    return 'dados recebido';
 });
 
 Route::get('/contato', function() {
